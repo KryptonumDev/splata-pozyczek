18953b790c0a3e66fd7c01366f9463acb1ccd797
kryptonumstudio <kryptonumstudio@gmail.com>
2022-08-29 17:28:25 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Spłata Pożyczek`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": ""
+    }
+  }, "gatsby-plugin-styled-components"]
+};
