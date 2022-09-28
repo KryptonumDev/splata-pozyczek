43415589f374c38d195219a4df8f8ae2731aeec2
kryptonumstudio <kryptonumstudio@gmail.com>
2022-09-28 15:04:22 +0200

    try without font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,9 +6,9 @@ import './src/styles/index.css'
 
 export const wrapPageElement = ({ element, props }) => (
     <Layout {...props}>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet"/> */}
         {element}
     </Layout>
 )
