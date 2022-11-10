550b76fea6e766945c7bd59ed18b7bf4127b06a5
kryptonumstudio <kryptonumstudio@gmail.com>
2022-11-10 12:56:24 +0100

    another font load method

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet";
 import styled from "styled-components"
 import GlobalStyles from "../../styles/global";
 import Footer from "./footer"
@@ -8,6 +9,27 @@ export default function Layout({ children }) {
 
     return (
         <Wrapper>
+            <Helmet>
+                <link rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin />
+
+                <link rel="preload"
+                    as="style"
+                    href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&display=swap" />
+
+                <link rel="preload"
+                    as="style"
+                    href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" />
+
+                <link rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&display=swap"
+                    media="print" onload="this.media='all'" />
+
+                <link rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap"
+                    media="print" onload="this.media='all'" />
+            </Helmet>
             <GlobalStyles />
             <Header />
             {children}
