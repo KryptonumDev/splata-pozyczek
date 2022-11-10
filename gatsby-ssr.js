d8608ec280155d67a738dfd564dce90661f04c11
kryptonumstudio <kryptonumstudio@gmail.com>
2022-11-10 13:20:40 +0100

    another font load try

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Layout from './src/components/layout'
 import './src/styles/normalize.css'
 
 export const wrapPageElement = ({ element, props }) => (
     <Layout {...props}>
+        <Helmet>
+            <link rel="preconnect"
+                href="https://fonts.gstatic.com"
+                crossorigin />
+
+            <link rel="preload"
+                as="style"
+                href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&display=swap" />
+
+            <link rel="preload"
+                as="style"
+                href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" />
+
+            <link rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&display=swap"
+                media="print" onload="this.media='all'" />
+
+            <link rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap"
+                media="print" onload="this.media='all'" />
+        </Helmet>
         {element}
     </Layout>
 )
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,7 @@
 import React from "react"
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet"
 import styled from "styled-components"
-import GlobalStyles from "../../styles/global";
+import GlobalStyles from "../../styles/global"
 import Footer from "./footer"
 import Header from "./header"
 
@@ -9,27 +9,6 @@ export default function Layout({ children }) {
 
     return (
         <Wrapper>
-            <Helmet>
-                <link rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossorigin />
-
-                <link rel="preload"
-                    as="style"
-                    href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&display=swap" />
-
-                <link rel="preload"
-                    as="style"
-                    href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" />
-
-                <link rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Arsenal:wght@400;700&display=swap"
-                    media="print" onload="this.media='all'" />
-
-                <link rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap"
-                    media="print" onload="this.media='all'" />
-            </Helmet>
             <GlobalStyles />
             <Header />
             {children}
