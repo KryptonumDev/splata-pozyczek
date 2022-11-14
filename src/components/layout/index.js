bec1f12a6d6e0a18ea8a0adb5bc1f909ea401fbe
kryptonumstudio <kryptonumstudio@gmail.com>
2022-11-14 17:41:53 +0100

    isolate footer and header

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import styled from "styled-components"
 import GlobalStyles from "../../styles/global"
-import Footer from "./footer"
-import Header from "./header"
+// import Footer from "./footer"
+// import Header from "./header"
 
 export default function Layout({ children }) {
 
