8567de7a665103ebdedce0044a7a85f858688398
KryptonumDev <kryptonumstudio@gmail.com>
2023-02-09 09:10:15 +0100

    Noindex removed

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -29,145 +29,180 @@ import OG from './../../static/og.jpg'
 export function Head({ pageContext, data: { wpPost: { id, seo, author, title, slug, blogPost } } }) {
   const canonical = 'https://splatapozyczek.pl' + pageContext.url
 
-return <>
-    <meta charSet="utf-8" />
-    {id === "cG9zdDoxMTM0"
-      ? null
-      : <meta name="robots" content="noindex" />}
-    <meta property="og:site_name" content={seo.opengraphSiteName} />
-    <meta name="google-site-verification" content="M2kghTKPmXOB2ezGLw7ShbO3sdW6rMn_uhsSVbHCt7I" />
-    <meta property="og:type" content='article' />
-    <meta property="article:modified_time" content={seo.opengraphModifiedTime} />
-    <meta property="article:published_time" content={seo.opengraphPublishedTime} />
-    <meta property="article:author" content={author.node.name} />
-    <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap_index.xml"></link>
+return (
+  <>
+    <meta charSet='utf-8' />
+    {id === 'cG9zdDoxMTM0' ? null : (
+      <meta
+        name='robots'
+        content='index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1'
+      />
+    )}
+    <meta property='og:site_name' content={seo.opengraphSiteName} />
+    <meta
+      name='google-site-verification'
+      content='M2kghTKPmXOB2ezGLw7ShbO3sdW6rMn_uhsSVbHCt7I'
+    />
+    <meta property='og:type' content='article' />
+    <meta
+      property='article:modified_time'
+      content={seo.opengraphModifiedTime}
+    />
+    <meta
+      property='article:published_time'
+      content={seo.opengraphPublishedTime}
+    />
+    <meta property='article:author' content={author.node.name} />
+    <link
+      rel='sitemap'
+      type='application/xml'
+      title='Sitemap'
+      href='/sitemap_index.xml'></link>
 
-    <script type="application/ld+json">
+    <script type='application/ld+json'>
       {JSON.stringify({
-        "@context": "https://schema.org",
-        "@type": "Article",
-        "mainEntityOfPage": {
-          "@type": "WebPage",
-          "@id": canonical
+        '@context': 'https://schema.org',
+        '@type': 'Article',
+        mainEntityOfPage: {
+          '@type': 'WebPage',
+          '@id': canonical
         },
-        "headline": title,
-        "description": blogPost.previewText,
-        "image": 'https://splatapozyczek.pl' + (seo.opengraphImage?.localFile?.publicURL ? seo.opengraphImage.localFile.publicURL : OG),
-        "author": {
-          "@type": "Person",
-          "name": author.node.name,
-          "url": 'https://splatapozyczek.pl/blog/' + slug + '/' // TODO
+        headline: title,
+        description: blogPost.previewText,
+        image:
+          'https://splatapozyczek.pl' +
+          (seo.opengraphImage?.localFile?.publicURL
+            ? seo.opengraphImage.localFile.publicURL
+            : OG),
+        author: {
+          '@type': 'Person',
+          name: author.node.name,
+          url: 'https://splatapozyczek.pl/blog/' + slug + '/' // TODO
         },
-        "publisher": {
-          "@type": "Organization",
-          "name": "Splata Pożyczek",
-          "logo": {
-            "@type": "ImageObject",
-            "url": Logo
+        publisher: {
+          '@type': 'Organization',
+          name: 'Splata Pożyczek',
+          logo: {
+            '@type': 'ImageObject',
+            url: Logo
           }
         },
-        "datePublished": seo.opengraphPublishedTime,
-        "dateModified": seo.opengraphModifiedTime
+        datePublished: seo.opengraphPublishedTime,
+        dateModified: seo.opengraphModifiedTime
       })}
     </script>
 
-    <script type="application/ld+json">
-      {JSON.stringify(
-        {
-          "@context": "https://schema.org",
-          "@graph": [
-            {
-              "@type": "Organization",
-              "@id": "https://splatapozyczek.pl/#organization",
-              "name": "Splatapozyczek.pl",
-              "url": "https://splatapozyczek.pl/",
-              "sameAs": [
-                "https://www.facebook.com/splatapozyczek",
-                "https://www.instagram.com/splatapozyczek.pl/",
-                "https://www.youtube.com/channel/UCdpboPGWbJy_e8Je_Xw9i9Q"
-              ],
-              "logo": {
-                "@type": "ImageObject",
-                "@id": "https://splatapozyczek.pl/#logo",
-                "inLanguage": "pl-PL",
-                "url": Logo,
-                "width": 181,
-                "height": 43,
-                "caption": "Splatapozyczek.pl"
-              },
-              "image": {
-                "@id": "https://splatapozyczek.pl/#logo"
+    <script type='application/ld+json'>
+      {JSON.stringify({
+        '@context': 'https://schema.org',
+        '@graph': [
+          {
+            '@type': 'Organization',
+            '@id': 'https://splatapozyczek.pl/#organization',
+            name: 'Splatapozyczek.pl',
+            url: 'https://splatapozyczek.pl/',
+            sameAs: [
+              'https://www.facebook.com/splatapozyczek',
+              'https://www.instagram.com/splatapozyczek.pl/',
+              'https://www.youtube.com/channel/UCdpboPGWbJy_e8Je_Xw9i9Q'
+            ],
+            logo: {
+              '@type': 'ImageObject',
+              '@id': 'https://splatapozyczek.pl/#logo',
+              inLanguage: 'pl-PL',
+              url: Logo,
+              width: 181,
+              height: 43,
+              caption: 'Splatapozyczek.pl'
+            },
+            image: {
+              '@id': 'https://splatapozyczek.pl/#logo'
+            }
+          },
+          {
+            '@type': 'WebSite',
+            '@id': 'https://splatapozyczek.pl/#website',
+            url: 'https://splatapozyczek.pl/',
+            name: 'Splatapozyczek.pl',
+            description:
+              'SplataPozyczek.pl \u2013 \u2705 kredyty got\u00f3wkowe, kredyt dla firm, po\u017cyczka konsolidacyjna, odd\u0142u\u017canie.',
+            publisher: { '@id': 'https://splatapozyczek.pl/#organization' },
+            potentialAction: [
+              {
+                '@type': 'SearchAction',
+                target: 'https://splatapozyczek.pl/?s={search_term_string}',
+                'query-input': 'required name=search_term_string'
               }
-            },
-            {
-              "@type": "WebSite",
-              "@id": "https://splatapozyczek.pl/#website",
-              "url": "https://splatapozyczek.pl/",
-              "name": "Splatapozyczek.pl",
-              "description": "SplataPozyczek.pl \u2013 \u2705 kredyty got\u00f3wkowe, kredyt dla firm, po\u017cyczka konsolidacyjna, odd\u0142u\u017canie.",
-              "publisher": { "@id": "https://splatapozyczek.pl/#organization" },
-              "potentialAction": [{ "@type": "SearchAction", "target": "https://splatapozyczek.pl/?s={search_term_string}", "query-input": "required name=search_term_string" }],
-              "inLanguage": "pl-PL"
-            },
-            {
-              "@type": "WebPage",
-              "@id": "https://splatapozyczek.pl/#webpage",
-              "url": "https://splatapozyczek.pl/",
-              "name": "Z nami uzyskasz nawet najtrudniejszy kredyt - Splatapozyczek.pl",
-              "isPartOf": { "@id": "https://splatapozyczek.pl/#website" },
-              "about": { "@id": "https://splatapozyczek.pl/#organization" },
-              "datePublished": "2015-11-18T08:37:53+00:00",
-              "dateModified": "2022-07-20T09:58:31+00:00",
-              "description": "Niestandardowa umowa pracownicza? Brak zdolno\u015bci kredytowej? Inne zobowi\u0105zania? Mo\u017cemy uzyska\u0107 kredyt dla Ciebie nawet w 24 H.",
-              "inLanguage": "pl-PL",
-              "potentialAction": [{ "@type": "ReadAction", "target": ["https://splatapozyczek.pl/"] }]
-            }]
-        }
-      )}
+            ],
+            inLanguage: 'pl-PL'
+          },
+          {
+            '@type': 'WebPage',
+            '@id': 'https://splatapozyczek.pl/#webpage',
+            url: 'https://splatapozyczek.pl/',
+            name: 'Z nami uzyskasz nawet najtrudniejszy kredyt - Splatapozyczek.pl',
+            isPartOf: { '@id': 'https://splatapozyczek.pl/#website' },
+            about: { '@id': 'https://splatapozyczek.pl/#organization' },
+            datePublished: '2015-11-18T08:37:53+00:00',
+            dateModified: '2022-07-20T09:58:31+00:00',
+            description:
+              'Niestandardowa umowa pracownicza? Brak zdolno\u015bci kredytowej? Inne zobowi\u0105zania? Mo\u017cemy uzyska\u0107 kredyt dla Ciebie nawet w 24 H.',
+            inLanguage: 'pl-PL',
+            potentialAction: [
+              { '@type': 'ReadAction', target: ['https://splatapozyczek.pl/'] }
+            ]
+          }
+        ]
+      })}
     </script>
 
-    {canonical
-      ? (
-        <>
-          <meta property="og:url" content={canonical} />
-        </>
-      )
-      : null}
+    {canonical ? (
+      <>
+        <meta property='og:url' content={canonical} />
+      </>
+    ) : null}
 
-    {seo?.title
-      ? (
-        <>
-          <meta property="twitter:title" content={seo.title} />
-          <meta property="og:title" content={seo.title} />
-        </>
-      )
-      : null}
+    {seo?.title ? (
+      <>
+        <meta property='twitter:title' content={seo.title} />
+        <meta property='og:title' content={seo.title} />
+      </>
+    ) : null}
 
-    {seo?.metaDesc
-      ? (
-        <>
-          <meta name="description" content={seo.metaDesc} />
-          <meta property="twitter:description" content={seo.metaDesc} />
-          <meta property="og:description" content={seo.metaDesc} />
-        </>
-      )
-      : null}
+    {seo?.metaDesc ? (
+      <>
+        <meta name='description' content={seo.metaDesc} />
+        <meta property='twitter:description' content={seo.metaDesc} />
+        <meta property='og:description' content={seo.metaDesc} />
+      </>
+    ) : null}
 
-    {seo.opengraphImage?.localFile?.publicURL
-      ? (
-        <>
-          <meta property="og:image" content={'https://splatapozyczek.pl' + seo.opengraphImage.localFile.publicURL} />
-          <meta property="twitter:image" content={'https://splatapozyczek.pl' + seo.opengraphImage.localFile.publicURL} />
-        </>
-      )
-      : (
-        <>
-          <meta property="og:image" content={'https://splatapozyczek.pl' + OG} />
-          <meta property="twitter:image" content={'https://splatapozyczek.pl' + OG} />
-        </>
-      )}
-
+    {seo.opengraphImage?.localFile?.publicURL ? (
+      <>
+        <meta
+          property='og:image'
+          content={
+            'https://splatapozyczek.pl' + seo.opengraphImage.localFile.publicURL
+          }
+        />
+        <meta
+          property='twitter:image'
+          content={
+            'https://splatapozyczek.pl' + seo.opengraphImage.localFile.publicURL
+          }
+        />
+      </>
+    ) : (
+      <>
+        <meta property='og:image' content={'https://splatapozyczek.pl' + OG} />
+        <meta
+          property='twitter:image'
+          content={'https://splatapozyczek.pl' + OG}
+        />
+      </>
+    )}
   </>
+)
 }
 
 export default function Post({ pageContext, data: { wpPost } }) {
