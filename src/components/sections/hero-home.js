e9a51821fa19372181e039c1e0ec6a70a99e7ad5
kryptonumstudio <kryptonumstudio@gmail.com>
2023-04-25 16:53:43 +0200

    add mr to text

--- src/components/sections/hero-home.js
+++ src/components/sections/hero-home.js
@@ -97,6 +97,10 @@ const Content = styled.div`
     padding-bottom: 160px;
     position: relative;
 
+    .h6{
+        margin-right: 6px;
+    }
+
     @media (max-width: 900px){
         padding-bottom: 0;
 
