bba316424baf07c149789715d1923e20defecfe3
kryptonumstudio <kryptonumstudio@gmail.com>
2023-04-25 11:20:01 +0200

    add email field

--- src/components/moleculas/forms/full.js
+++ src/components/moleculas/forms/full.js
@@ -121,13 +121,13 @@ export default function Form({ ip, extended, title, type, setIsSended, typTemato
                     }
                 </div>
                 <div className="flex">
-                    {/* <LabelInput
+                    <LabelInput
                         name='email'
                         label='Adres e-mail*'
                         params={{ required: true, pattern: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ }}
                         register={register}
                         errors={errors}
-                    />*/}
+                    />
                     <LabelInput
                         name='phone'
                         label='Numer telefonu*'
