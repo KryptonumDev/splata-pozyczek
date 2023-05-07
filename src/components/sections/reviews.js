ee34d63b5872bbd653a7a398c7a80b5c8a791479
kryptonumstudio <kryptonumstudio@gmail.com>
2023-05-07 21:11:15 +0200

    add comments

--- src/components/sections/reviews.js
+++ src/components/sections/reviews.js
@@ -8,37 +8,19 @@ import axios from "axios"
 
 export default function Reviews({ data: { title, text }, expert, comments }) {
 
-    const { allWpComment } = useStaticQuery(graphql`
+    const { wpPage: { reviews: { reviewsRepeater } } } = useStaticQuery(graphql`
     query {
-        allWpComment(filter: {comment: {isShown: {eq: true}}}) {
-          nodes {
-            author {
-              node {
-                name
-              }
+        wpPage(id: {eq: "cG9zdDo2MzQ="}) {
+            reviews {
+                reviewsRepeater {
+                    text
+                    author
+                }
             }
-            content
-          }
         }
       }
   `)
 
-    const [chosenComments, setComments] = useState([])
-
-    useLayoutEffect(() => {
-        axios('https://www-data.splatapozyczek.pl/wp-json/wp/v2/comments')
-            .then(response => {
-                setComments(response.data.filter(el => el.acf.is_shown === true))
-            })
-
-        axios(`https://mybusiness.googleapis.com/v4/accounts/6c4f4c800d8b88b5/locations/ChIJyyceLHDNHkcRtYiLDYBMT2w/reviews`, {
-            headers: {
-                'Authorization': `Bearer ${'AIzaSyCFevQz4QmtyPsfNDy4HU12UAREihWLxGc'}`
-            }
-        }).then(res => {
-            debugger
-        })
-    }, [])
 
     const [showCount, changeShowCount] = useState(6)
 
@@ -51,7 +33,7 @@ export default function Reviews({ data: { title, text }, expert, comments }) {
                     : null}
 
                 <Grid>
-                    {chosenComments?.map((el, index) => {
+                    {reviewsRepeater?.map((el, index) => {
                         if (index > showCount - 1) {
                             return null
                         }
@@ -62,16 +44,16 @@ export default function Reviews({ data: { title, text }, expert, comments }) {
                                     <path d="M0.5 27.744C2.676 25.824 4.468 23.552 5.876 20.928C7.284 18.304 8.02 15.776 8.084 13.344H7.412C5.62 13.344 4.084 12.768 2.804 11.616C1.524 10.336 0.884 8.704 0.884 6.72C0.884 4.672 1.524 3.008 2.804 1.728C4.084 0.576 5.62 0 7.412 0C9.14 0 10.676 0.576 12.02 1.728L12.308 2.112C13.844 3.84 14.612 6.272 14.612 9.408C14.612 13.12 13.556 17.088 11.444 21.312C9.396 25.472 6.868 28.8 3.86 31.296L0.5 27.744ZM18.452 27.744C20.628 25.824 22.42 23.552 23.828 20.928C25.236 18.304 25.972 15.776 26.036 13.344H25.364C23.508 13.344 21.972 12.768 20.756 11.616C19.476 10.336 18.836 8.704 18.836 6.72C18.836 4.672 19.476 3.008 20.756 1.728C21.972 0.576 23.508 0 25.364 0C27.092 0 28.628 0.576 29.972 1.728L30.26 2.112C31.796 3.84 32.564 6.272 32.564 9.408C32.564 13.12 31.508 17.088 29.396 21.312C27.348 25.472 24.82 28.8 21.812 31.296L18.452 27.744Z" fill="#DAE2FF" />
                                 </svg>
                                 <figure >
-                                    <blockquote className="body3" dangerouslySetInnerHTML={{ __html: el.content.rendered }} />
-                                    <figcaption className="body2">{el.author_name}</figcaption>
+                                    <blockquote className="body3" dangerouslySetInnerHTML={{ __html: el.text }} />
+                                    <figcaption className="body2">{el.author}</figcaption>
                                 </figure >
                             </Item>
                         )
                     })}
                 </Grid>
-                {chosenComments.length < 6
+                {reviewsRepeater.length < 6
                     ? null
-                    : showCount > chosenComments.length
+                    : showCount > reviewsRepeater.length
                         ? <OutlinedButton as='button' onClick={() => { changeShowCount(6) }} className="button">Ukryj</OutlinedButton>
                         : <OutlinedButton as='button' onClick={() => { changeShowCount(showCount + 6) }} className="button">Wczytaj więcej</OutlinedButton>
                 }
