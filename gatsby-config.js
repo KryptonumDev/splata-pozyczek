38b3354f57a5827f8b9ac38e24e970c3b9b6d53c
Koniec <d3ntka@gmail.com>
2024-05-08 15:38:22 +0200

    add local optimizations and debug options

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,16 +21,40 @@ module.exports = {
     {
       resolve: 'gatsby-source-wordpress',
       options: {
+        verbose: true,
+        debug: {
+          preview: true,
+          timeBuildSteps: true,
+          graphql: {
+            showQueryVarsOnError: true,
+            writeQueriesToDisk: false,
+          },
+        },
         schema: {
           timeout: 3000000,
           perPage: 20, // currently set to 100
           requestConcurrency: 5, // currently set to 15
           previewRequestConcurrency: 2, // currently set to 5
         },
-        "url": "https://www-data.splatapozyczek.pl/graphql"
-      }
-    }, 
-    'gatsby-plugin-image',
+        url: "https://www-data.splatapozyczek.pl/graphql",
+        develop: {
+          nodeUpdateInterval: 3600000,
+          hardCacheMediaFiles: true,
+          hardCacheData: true,
+        },
+        type: {
+          MediaItem: {
+            localFile: {
+              requestConcurrency: 60,
+            },
+          },
+          Post: {
+            limit: process.env.NODE_ENV === `development` ? 50 : null,
+          },
+        },
+      },
+    },
+    "gatsby-plugin-image",
     {
       resolve: `gatsby-plugin-sharp`,
       options: {
