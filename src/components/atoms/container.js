94f8d6755653b53537f3b14f5776868d58531004
Mariusz <mariusz@efectownia.pl>
2024-11-15 15:13:42 +0100

    feat: predictable container

--- src/components/atoms/container.js
+++ src/components/atoms/container.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    max-width: 1200px;
-    box-sizing: content-box;
-    padding: 0 clamp(16px, ${24 / 768 * 100}vw, 80px);
+    max-width: 1328px;
+    padding: 0 2rem;
     margin: 0 auto;
     position: relative;
+    
+    @media (min-width: 768px){
+        padding: 0 4rem;
+    }
 
     &.small{
         max-width: 1160px;
