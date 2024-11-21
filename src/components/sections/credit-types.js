5c2414c38ac8436fcd708bd07d0c5a38f12e4055
Mariusz <mariusz@efectownia.pl>
2024-11-21 10:55:06 +0100

    fix

--- src/components/sections/credit-types.js
+++ src/components/sections/credit-types.js
@@ -163,7 +163,7 @@ const ControlButtonsWrap = styled.div`
 `
 
 const ControlWrap = styled.div`
-    max-width: var(--inner-container-width);
+    max-width: 1000px;
     margin: 0 auto;
     overflow: hidden;
 
@@ -284,7 +284,6 @@ const Control = styled(motion.div)`
 `
 
 const Content = styled.div`
-    max-width: 1136px;
     margin: 0 auto;
 
     .h4{
